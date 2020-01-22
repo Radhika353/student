00dfb3aa9b4cc88cca34a570640bd073baed525f
Radhika353 <59924433+Radhika353@users.noreply.github.com>
2020-01-22 09:29:48 -0500

    Update Student.java

--- src/ca/sheridancollege/week1/softwarefundamentals/Student.java
+++ src/ca/sheridancollege/week1/softwarefundamentals/Student.java
@@ -12,7 +12,7 @@ package ca.sheridancollege.week1.softwarefundamentals;
 public class Student {
     
     private String name;
-    private double GPA;
+   // private double GPA;
     private String email;
     private String ID;
     private String student;
